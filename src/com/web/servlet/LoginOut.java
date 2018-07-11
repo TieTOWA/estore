434fe3f18981024f236baa0f933d749ed42f23fc
Jie Chonghai <3357991893@qq.com>
2018-07-11 20:10:49 +0800

    登出商城

---
+++ src/com/web/servlet/LoginOut.java
@@ -0,0 +1,34 @@
+package com.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/index.jsp")
+public class LoginOut extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public LoginOut() {
+        super();
+
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		session.invalidate();
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
